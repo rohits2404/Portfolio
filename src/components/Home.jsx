1188e9d45f7de6657e243bfe5117efd6e307981c
Rohit Sharma <rohits8731@gmail.com>
2025-05-15 18:39:48 +0530

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
                                 </p>
                                 <div className="d-flex">
                                 <Link
-                                    to="/"
+                                    to="https://drive.google.com/file/d/1AFLoOU3HcsUufMms6afuSxaL9URIaXh_/view?usp=sharing"
                                     className=" border-0 btns d-flex justify-content-around align-items-center"
                                     style={{
                                     backgroundColor: "#cacccb",
